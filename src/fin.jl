d62afc2f32833baec8fc932ea886afa7951c1fc0
Sean Baskin <seanbaskin@gmail.com>
2019-12-23 15:11:16 -0700

    Updated to properly reference submodules

--- src/fin.jl
+++ src/fin.jl
@@ -1,6 +1,12 @@
 module fin
 
-include("Budget.jl")
+# Include external Julia files and modules
+include("FinancePlan.jl")
+include("Income.jl")
 include("Statement.jl")
+
+# todo adding to namespace
+using .FinancePlan
+using .Income
 
 end # module
