0af75f6a807a6bfd486a66d65c9b544bc68f6086
Sean Baskin <seanbaskin@gmail.com>
2020-01-20 18:02:28 -0700

    Added additional composite types to the accounting module

--- src/Accounting.jl
+++ src/Accounting.jl
@@ -1,4 +1,8 @@
-module Accouting
+module Accounting
+
+import Dates
+
+using Dates
 
 abstract type Asset end
 abstract type Cash <: Asset end
@@ -6,13 +10,33 @@ abstract type BankAccount <: Cash end
 abstract type PersonalProperty <: Asset end
 abstract type InvestedAssets <: Asset end
 
+"""
+A liability is a present obligation of an individual arising from past events,
+the settlement of which is expected to result in an outflow from the individual
+of resources embodying economic benefits.
+"""
 abstract type Liability end
 
+"An outflow of cash or other assets from an individual to another entity."
+abstract type Expenditure end
 
+"Represents an individual's liquid cash."
 struct LiquidCash <: Cash end
+
+"Represents a checking account at a financial institution."
 struct CheckingAccount <: BankAccount end
+
+"Represents a savings account at a financial institution."
 struct SavingsAccount <: BankAccount end
 
+"Represents a budgeted expense."
+struct BudgetExpense <: Expenditure
+    amount::Float64
+    category::String
+    due::Date
+    # weight::Int16
+end
 
+export BudgetExpense, Expenditure
 
 end
